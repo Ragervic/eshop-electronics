8f0a39fad699713fec3401576586c1faf7577564
Ragervic <141401970+Ragervic@users.noreply.github.com>
2025-08-01 19:35:35 +0300

    RegisterPage

--- Models/SiteSettings.cs
+++ Models/SiteSettings.cs
@@ -5,10 +5,10 @@ namespace TestP.Models
     public class SiteConfiguration
     {
         public string ContactEmail { get; set; } = "support@ecomadmin.com";
-        public string ContactPhone { get; set; } = "+254 712 345 689"; 
-        public string CompanyAddress { get; set; } = "123 Lenana Street,Jumuia, Kilimani, Nairobi, 12345"; 
+        public string ContactPhone { get; set; } = "+254 712 345 689";
+        public string CompanyAddress { get; set; } = "123 Lenana Street,Jumuia, Kilimani, Nairobi, 12345";
         public string? CompanyLogo { get; set; }
-        public string? WebsiteBanner { get; set; } 
+        public string? WebsiteBanner { get; set; }
         public string ShippingPolicyDescription { get; set; } = "Provide clear and concise information about your shipping methods, delivery times, and costs. Include details on international shipping, tracking, and any restrictions.";
         public decimal StandardShippingCost { get; set; } = 5.00M;
         public decimal ExpressShippingCost { get; set; } = 15.00M;
